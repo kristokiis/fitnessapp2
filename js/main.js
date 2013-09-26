5663056c25958411b74b47b7a89747317f4e08a8
Kristo Kiis <kristo@efley.ee>
2013-09-26 15:13:56 +0300

    old android support

--- js/main.js
+++ js/main.js
@@ -13,6 +13,15 @@ animateCallback : function(){},
 extraSpace : 20,
 limit: 1000
 }, options);
+
+if( ua.indexOf("Android") >= 0 ) {
+	var androidversion = parseFloat(ua.slice(ua.indexOf("Android")+8)); 
+	//alert(androidversion);
+	if (androidversion <= 2.3) {
+		link.href = 'css/main_a23.css';
+		head.appendChild(link);
+	} 
+} 
  
 // Only textarea's auto-resize:
 this.filter('textarea').each(function(){
